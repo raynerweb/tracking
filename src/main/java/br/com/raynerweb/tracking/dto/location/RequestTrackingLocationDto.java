b1b5f6ca57853f768ff09b106301b622bfaa6238
raynerweb <raynerweb@gmail.com>
2023-02-20 21:07:18 +0000

    validation on tracking geolocation

--- src/main/java/br/com/raynerweb/tracking/dto/location/RequestTrackingLocationDto.java
+++ src/main/java/br/com/raynerweb/tracking/dto/location/RequestTrackingLocationDto.java
@@ -1,10 +1,15 @@
 package br.com.raynerweb.tracking.dto.location;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
 public record RequestTrackingLocationDto(
+        @NotNull(message = "Vehicle is required")
         Long vehicleId,
+        @NotNull(message = "Latitude is required")
         BigDecimal latitude,
+        @NotNull(message = "Longitude is required")
         BigDecimal longitude
 ) {
 }
