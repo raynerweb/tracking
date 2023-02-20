7d449120812871eeadbfa6e85c814a46d53d0ae7
raynerweb <raynerweb@gmail.com>
2023-02-20 22:27:47 +0000

    add swagger

--- src/main/java/br/com/raynerweb/tracking/rest/TrackingSensorRest.java
+++ src/main/java/br/com/raynerweb/tracking/rest/TrackingSensorRest.java
@@ -1,29 +1,34 @@
 package br.com.raynerweb.tracking.rest;
 
+import br.com.raynerweb.tracking.dto.error.ResponseValidationError;
+import br.com.raynerweb.tracking.dto.location.ResponseTrackingLocationDto;
 import br.com.raynerweb.tracking.dto.sensor.RequestTrackingSensorDto;
 import br.com.raynerweb.tracking.dto.sensor.ResponseTrackingSensorDto;
-import br.com.raynerweb.tracking.service.TrackingSensorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("tracking-sensor")
-public class TrackingSensorRest {
+@Tag(name = "Tracking Sensor", description = "Tracking Sensor Endpoint")
+public interface TrackingSensorRest {
+    @Operation(summary = "Fetch Tracking Sensor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Fetch Tracking Sensor",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseTrackingLocationDto[].class))})
+    })
+    List<ResponseTrackingSensorDto> findAll();
 
-    @Autowired
-    private TrackingSensorService service;
-
-    @GetMapping
-    public List<ResponseTrackingSensorDto> findAll() {
-        return service.findAll();
-    }
-
-    @PostMapping
-    public void save(@Valid
-                     @RequestBody RequestTrackingSensorDto dto) {
-        service.saveAsync(dto);
-    }
+    @Operation(summary = "Save Tracking Sensor")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Save Tracking Sensor"),
+            @ApiResponse(responseCode = "400", description = "Input Validation",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseValidationError.class)))})
+    void save(@Valid RequestTrackingSensorDto dto);
 }
---
+++ src/main/java/br/com/raynerweb/tracking/rest/impl/TrackingSensorRestImpl.java
@@ -0,0 +1,28 @@
+package br.com.raynerweb.tracking.rest.impl;
+
+import br.com.raynerweb.tracking.dto.sensor.RequestTrackingSensorDto;
+import br.com.raynerweb.tracking.dto.sensor.ResponseTrackingSensorDto;
+import br.com.raynerweb.tracking.rest.TrackingSensorRest;
+import br.com.raynerweb.tracking.service.TrackingSensorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("tracking-sensor")
+public class TrackingSensorRestImpl implements TrackingSensorRest {
+
+    @Autowired
+    private TrackingSensorService service;
+
+    @GetMapping
+    public List<ResponseTrackingSensorDto> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public void save(@RequestBody RequestTrackingSensorDto dto) {
+        service.saveAsync(dto);
+    }
+}
